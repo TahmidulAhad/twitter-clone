9d5fdcf4deae808f958b95c012905339874cbd51
Md. Tahmidul Alam Ahad <mdtahmidula@gmail.com>
2026-08-08 01:37:48 +0600

    Add User Class

---
+++ TwitterClone/TwitterClone.Domain/User.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitterClone.Domain
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string UserName { get; private set; }
+        public string Email { get; private set; }
+
+        public User(string userName, string email)
+        {
+            Id = Guid.NewGuid();
+            UserName = userName;
+            Email = email;
+        }
+    }
+}
