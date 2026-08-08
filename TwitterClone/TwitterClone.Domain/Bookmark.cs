13bce6b78722627c81aff568f6d1bff5cbc798a0
Md. Tahmidul Alam Ahad <mdtahmidula@gmail.com>
2026-08-09 01:57:59 +0600

    Add Bookmark Class

---
+++ TwitterClone/TwitterClone.Domain/Bookmark.cs
@@ -0,0 +1,18 @@
+﻿namespace TwitterClone.Domain
+{
+    public class Bookmark
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid TweetId { get; private set; }
+        public DateTime BookmarkedAt { get; private set; }
+
+        public Bookmark(Guid userId, Guid tweetId)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            TweetId = tweetId;
+            BookmarkedAt = DateTime.UtcNow;
+        }
+    }
+}
