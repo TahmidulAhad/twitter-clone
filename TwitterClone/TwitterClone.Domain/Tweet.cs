f4f26b9f1fff17106ff90ad5216cc069978143fa
Md. Tahmidul Alam Ahad <mdtahmidula@gmail.com>
2026-08-09 00:00:17 +0600

    Add Tweet Class

---
+++ TwitterClone/TwitterClone.Domain/Tweet.cs
@@ -0,0 +1,27 @@
+﻿namespace TwitterClone.Domain
+{
+    public class Tweet
+    {
+        public Guid Id { get; private set; }
+        public Guid AuthorId { get; private set; }
+        public string Content {  get; private set; }
+
+        public Tweet(Guid authorId, string content)
+        {
+            Id = Guid.NewGuid();
+            AuthorId = authorId;
+            SetContent(content);
+        }
+
+        private void SetContent(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                throw new ArgumentException("Tweet cannot be empty.");
+            if (content.Length > 280)
+                throw new ArgumentException("Tweet cannot exceed 280 characters.");
+
+            Content = content;
+        }
+    }
+
+}
